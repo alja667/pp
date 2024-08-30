9ef6618e7138a5a42132c328209dec329e51520a
alja667 <aprogozhina@gmail.com>
2024-08-30 23:31:46 +0300

    Add files via upload

---
+++ переменная.py
@@ -0,0 +1,5 @@
+number_dz=12
+spent_hours=1.5
+name_course="Python"
+time_of_gtask= spent_hours/number_dz
+print("Курс:",name_course,",","всего задач:",number_dz,",","затрачено часов:",spent_hours,",","среднее время выполнения:",time_of_gtask)
